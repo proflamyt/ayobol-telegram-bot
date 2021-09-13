6afcea3dcdab92627d38b340373b83f9e78613c2
taofesco <36924079+taofesco@users.noreply.github.com>
2021-09-13 22:00:57 +0100

    chatiebot

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1863320941:AAGyEx7esksY7e4rw1zPxbdr1mSV1m_3OPc"
 bot_user_name = "sheyebot"
-URL = "https://git.heroku.com/nameless-peak-46012.git"
+URL = "https://git.heroku.com/chatiebot.git"
