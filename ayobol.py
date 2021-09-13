40d0db2846a1cb9db49f646ad16896d0d970af24
olawale abdulrasheed <proflamyt@gmail.com>
2021-09-13 12:21:15 -0400

    Ready to deploy

--- ayobol.py
+++ ayobol.py
@@ -2,7 +2,7 @@ import re
 import telegram
 from telegram.ext.filters import Filters
 import speech_recognition as sr
-import ftransc.core as fr
+#import ftransc.core as fr
 from flask import Flask, request, session , copy_current_request_context
 from telebot.credentials import bot_token, bot_user_name,URL
 from flask_socketio import SocketIO, emit, disconnect
@@ -24,64 +24,7 @@ app.config['SECRET_KEY'] = 'secret!'
 socket_ = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
-my_bot  = ChatBot(
-'Ayobol',
-storage_adapter='chatterbot.storage.SQLStorageAdapter',
-logic_adapters=[
-'chatterbot.logic.MathematicalEvaluation',
-'chatterbot.logic.BestMatch'
-],
-database_uri='sqlite:///database.sqlite3'
-)
 
-small_talk = ['bawo ni?',
-    'enle o!',
-    'Eka ro?',
-    'Eka san',
-    'Bawo ni nibe yen o',
-    "Pẹlẹ o ", " Bawo ni o se wa?", " Pẹlẹ o bawo ni ", " Bawo ni nibe yen o ", " kilode ",
-    "omo odun melo ni e?", " nigba wo ni ojo ibi re?", " Nigbawo ni wọn bi ọ?",
-    "Mo wa ni gbogbo ọsẹ ", " mi o ni eto kankan ", " n ko lowo loni ",
-    'Bawo ni o ṣe n ṣe',
-    'Bawo ni nibe yen o',
-    'mo wa dada',
-    'o dara o ṣeun.',
-    'inu mi dun ati ba e soro',
-    'kini mole shey fun e',
-    'bami ki ile o',
-    'eyaaa pele omo mi',
-    'ki ni oruko e?',
-    'ayobol ni oruko mi .mo ma ran e lowo pelu ile e']
-commands1 = ['pa fan', "mo ti tan fan " ,
-        'moti pa fan yen',"fan ti wa ni pipade", "mo to pa fan"]
-age= ["omo odun melo ni e?",  " nigba wo ni ojo ibi re?", " Nigbawo ni wọn bi ọ?", "omo odun merinlelogun ni mi", " A bi mi ni ọdun 1996", "Ọjọ -ibi mi jẹ Oṣu Keje Ọjọ 3 ati pe a bi mi ni ọdun 1996", "03/07/1996"]
-
-goodbye  = [
-" o dabọ ","ki ile o", 'ka sun re', " ri ọ ", " titi a o tun pade ", "nigbamii","O dara lati ba ọ sọrọ ", " Ma a ri e laipe ", " Sọ laipẹ!"
-]
-commands2 = ['tan ino',
-        'ino ti tan', "ina ti tan ", "mo ti pa ina naa", " ololufẹ naa wa ni titan "]
-
-
-
-
-list_trainer = ListTrainer(my_bot)
-for item in (small_talk, commands1,commands2, age, goodbye):
-    list_trainer.train(item)
-
-
-
-def ai(msg):
-    res = my_bot.get_response(msg)
-    
-    if 'ino' in msg:
-        return {'response': res, 'command' :1}
-    if 'fan' in msg:
-        return {'response': res, 'command' :2}
-    if 'ilekun' in msg:
-        return {'response': res, 'command' :3}
-
-    return {'response': res, 'command' : 0}
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def respond():
@@ -111,7 +54,7 @@ def respond():
 
         voice = bot.getFile(update.message.voice.file_id)
     
-        fr.transcode(voice.download('file.ogg'), 'wav')
+        #fr.transcode(voice.download('file.ogg'), 'wav')
         r = sr.Recognizer()
 
         with sr.WavFile('file.wav') as source :
@@ -119,7 +62,7 @@ def respond():
 
             try: 
                 txt = r.recognize_google(audio) 
-                txt =  my_bot.get_response(txt)
+                #txt =  my_bot.get_response(txt)
             except sr.UnknownValueError as e:
                 print(f'Speech to Text Service could not generate request {e}')
             
