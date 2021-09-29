04b3330f0b7d4866198a4a798070dadc371a3286
CRUZIII <proflamyt>
2021-09-29 14:11:17 +0800

    changed url

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1863320941:AAGyEx7esksY7e4rw1zPxbdr1mSV1m_3OPc"
 bot_user_name = "sheyebot"
-URL = "https://chatiebot.herokuapp.com/"
+URL = "https://nameless-peak-46012.herokuapp.com/"
